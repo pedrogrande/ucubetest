3d3e921f7572092e5d648641fe06a30779995495
pedrogrande <pete@peterargent.com>
2023-01-27 11:35:13 +1100

    removed hosts config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "*.railway.app", "*.herokuapp.com"
+  # config.hosts << "*.railway.app", "*.herokuapp.com"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
